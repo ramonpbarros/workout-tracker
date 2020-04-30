693ddc267f573f230987e7968374f839bd0a0c11
Sorrab <ramonpbarros@gmail.com>
2020-04-29 22:20:33 -0700

    added module

--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const mongojs = require("mongojs");
 
 // TODO: import required model/s
 const Workout = require("../models/workout");
@@ -7,7 +8,7 @@ const Workout = require("../models/workout");
 
 // Creates a workout using data in the request body.
 router.post("/api/workouts", (req, res) => {
-  Workout.create(req)
+  Workout.create(req.body)
     .then((dbWorkout) => {
       res.json(dbWorkout);
     })
@@ -19,22 +20,51 @@ router.post("/api/workouts", (req, res) => {
 // Respond with workout for id url parameter. This should
 // respond with the updated workout json
 router.put("/api/workouts/:id", (req, res) => {
-  // CODE HERE
+  console.log(req.body);
+  Workout.update(
+    { _id: mongojs.ObjectId(req.params.id) },
+    { $set: req.body },
+    (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.json(data);
+      }
+    }
+  );
 });
 
 // Respond with json for all the workouts in an array.
 router.get("/api/workouts", (req, res) => {
-  // CODE HERE
+  Workout.find((err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(data);
+    }
+  });
 });
 
 // Respond with json array containing the last 7 workouts
 router.get("/api/workouts/range", (req, res) => {
-  // CODE HERE
+  Workout.find().limit(7, (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(data);
+    }
+  });
 });
 
 // Delete workout with id matching id in the request body.
-router.delete("/api/workouts", (req, res) => {
-  // CODE HERE
+router.delete("/api/workouts/:id", (req, res) => {
+  Workout.remove({ _id: mongojs.ObjectId(req.params.id) }, (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(data);
+    }
+  });
 });
 
 module.exports = router;
