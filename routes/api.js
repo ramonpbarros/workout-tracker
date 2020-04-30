770f07f1164cdd54626415c6c28a5e8132d2efd2
Sorrab <ramonpbarros@gmail.com>
2020-04-30 10:02:20 -0700

    fixed routes

--- routes/api.js
+++ routes/api.js
@@ -12,50 +12,49 @@ router.post("/api/workouts", (req, res) => {
       res.json(dbWorkout);
     })
     .catch((err) => {
-      res.json(err);
+      res.status(400).json(err);
     });
 });
 
 // Respond with workout for id url parameter. This should
 // respond with the updated workout json
 router.put("/api/workouts/:id", (req, res) => {
-  console.log(req.body);
-  db.Workout.update({ _id: req.params.id }, { $set: req.body }, (err, data) => {
-    if (err) {
-      console.log(err);
-    } else {
+  db.Workout.findIdAndUpdate(req.params.id, { $$push: { exercises: req.body } })
+    .then((data) => {
       res.json(data);
-    }
-  });
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
 });
 
 // Respond with json for all the workouts in an array.
 router.get("/api/workouts", (req, res) => {
-  db.Workout.find((err, data) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.json(data);
-    }
-  });
+  db.Workout.find({})
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
 });
 
 // Respond with json array containing the last 7 workouts
 router.get("/api/workouts/range", (req, res) => {
-  db.Workout.find()
+  db.Workout.find({})
+    .limit(7)
     .sort({ day: -1 })
-    .limit(7, (err, data) => {
-      if (err) {
-        console.log(err);
-      } else {
-        res.json(data);
-      }
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
     });
 });
 
 // Delete workout with id matching id in the request body.
 router.delete("/api/workouts/:id", (req, res) => {
-  db.Workout.remove({ _id: req.params.id }, (err, data) => {
+  db.Workout.remove(req.params.id, (err, data) => {
     if (err) {
       console.log(err);
     } else {
